00c76a0ebd1c9fa7119d461cda9d94d6b963397d
Tejas mayekar <tejasmayekar13@gmail.com>
2023-06-12 16:26:40 +0530

    products

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -1,18 +1,36 @@
+'use client'
+
+import Card from "./cards"
+
 export default function ProductsList(params:any) {
     return(
-        <div className="h-min  p-16 flex">
-            <div className="w-1/4 p-4">
-                <select name="cars" id="cars" className="w-full p-3 text-xl bg-white shadow-lg rounded-lg">
+        <div className="h-min bg-slate-100 p-16 flex lg:md:sm:flex-row flex-col">
+            <div className="lg:md:sm:w-1/4 w-full h-max sm:p-4 py-4 flex lg:md:sm:flex-col justify-between">
+                <select name="cars" id="cars" className="w-1/3 sm:w-full my-2 p-3 text-xl bg-white shadow-lg rounded-lg">
                     <option value="volvo">Volvo</option>
                     <option value="saab">Saab</option>
                     <option value="mercedes">Mercedes</option>
                     <option value="audi">Audi</option>
                 </select>
-                <div className="">
-
-                </div>
+                <select name="cars" id="cars" className="w-1/3 sm:w-full my-2 p-3 text-xl bg-white shadow-lg rounded-lg">
+                    {
+                        [1,2,3,4,5,6,7,8,9].map((item)=>{
+                            return(
+                                <option key={item} value={item}>{item}</option>
+                            )
+                        })
+                    }
+                </select>
             </div>
-            <div className="bg-slate-100 w-full p-4">Option</div>
+            <div className="bg-white w-full p-6 grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 gap-4">
+                {
+                    [1,2,3,4,5,6,7,8,9,10,11,12,13,14].map((item)=>{
+                        return(
+                            <Card key={item}/>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <div className="h-screen">
       <div className="h-screen p-10 flex">
-        <div className="sm:w-1/2 sm:p-14 flex flex-col justify-center">
+        <div className="lg:md:sm:w-1/2 sm:p-14 flex flex-col justify-center">
           <h1 className="text-6xl font-semibold">Find The Best Style That Suits You</h1>
           <p className="text-3xl py-10 text-justify">{para}</p>
           <button className="text-3xl font-medium bg-slate-200 px-6 py-4 shadow-lg w-max sm:animate-none animate-pulse">SHOP NOW</button>
         </div>
-        <div className="w-1/2 h-full sm:block hidden bg-[url('https://i.pinimg.com/originals/ab/36/16/ab36169df7a3817916ad75c3f5b76900.gif')] bg-cover bg-no-repeat "></div>
+        <div className="w-1/2 h-full lg:md:sm:block hidden bg-[url('https://i.pinimg.com/originals/ab/36/16/ab36169df7a3817916ad75c3f5b76900.gif')] bg-cover bg-no-repeat "></div>
       </div>
       <NewCollection/> 
       <DealsOfMonth/>
